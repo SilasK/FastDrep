c10a865b97ec75c0d58e811ac62bc82dd012174f
Silas Kieser <silas.kieser@gmail.com>
2019-08-29 16:47:51 +0200

    add read bbsketch

--- scripts/common/io.py
+++ scripts/common/io.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 def simplify_path(path,remove_gz=True):
     """Removes dir and extension from a filepath.
@@ -10,3 +11,30 @@ def simplify_path(path,remove_gz=True):
         name=  os.path.splitext(name)[0]
 
     return name
+
+
+def convert_percentages(df):
+    """Convet all columns with strings and % at the end to percentages
+    """
+    for col in df.columns:
+        if df.dtypes[col]=='object':
+            if df[col].iloc[0].endswith('%'):
+                df.loc[:,col]= df[col].str.rstrip('%').astype('float') / 100.0
+
+def read_sendsketch(send_sketch_file):
+    """ reads output of sendsketch.sh
+        Format2 (default)
+        parses parameters in first line
+    """
+    f= open(send_sketch_file)
+    f.readline() # trash empty line
+    comment_line= f.readline().strip()
+    params= dict( key_value.split(':')  for key_value in comment_line.split('\t'))
+
+    df= pd.read_csv(f,sep='\t')
+
+    convert_percentages(df)
+
+
+
+    return df,params
