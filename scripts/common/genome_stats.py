0d77231cb28f8b5cbfeb70a15eb5832e471739f0
kiesers <silas.kieser@gmail.com>
2020-02-13 15:45:01 +0100

    exception handling

--- scripts/common/genome_stats.py
+++ scripts/common/genome_stats.py
@@ -12,41 +12,46 @@ def genome_stats(fasta_file,remove_index=True):
        name,Length, n_seq,N50
     """
 
-    name = simplify_path(fasta_file)
+    try:
+
+        name = simplify_path(fasta_file)
 
 
-    lengths = []
+        lengths = []
 
-    with open(fasta_file) as fasta:
-        ## parse each sequence by header: groupby(data, key)
-        faiter = (x[1] for x in groupby(fasta, lambda line: line[0] == ">"))
+        with open(fasta_file) as fasta:
+            ## parse each sequence by header: groupby(data, key)
+            faiter = (x[1] for x in groupby(fasta, lambda line: line[0] == ">"))
 
-        for record in faiter:
-            ## join sequence lines
-            seqlen = sum(len(s.strip()) for s in faiter.__next__())
-            lengths.append(seqlen)
+            for record in faiter:
+                ## join sequence lines
+                seqlen = sum(len(s.strip()) for s in faiter.__next__())
+                lengths.append(seqlen)
 
-    ## sort contigs longest>shortest
-    all_len=sorted(lengths, reverse=True)
-    csum=np.cumsum(all_len)
+        ## sort contigs longest>shortest
+        all_len=sorted(lengths, reverse=True)
+        csum=np.cumsum(all_len)
 
-    Length = int(sum(lengths))
-    n_seq = len(lengths)
+        Length = int(sum(lengths))
+        n_seq = len(lengths)
 
-    n2=int(Length/2)
+        n2=int(Length/2)
 
-    # get index for cumsum >= N/2
-    csumn2=min(csum[csum >= n2])
-    ind=int(np.where(csum == csumn2)[0][0])
+        # get index for cumsum >= N/2
+        csumn2=min(csum[csum >= n2])
+        ind=int(np.where(csum == csumn2)[0][0])
 
-    N50 = all_len[ind]
-    ## N90
-    #nx90=int(sum(lengths)*0.90)
+        N50 = all_len[ind]
+        ## N90
+        #nx90=int(sum(lengths)*0.90)
 
-    ## index for csumsum >= 0.9*N
-    #csumn90=min(csum[csum >= nx90])
-    #ind90=numpy.where(csum == csumn90)
-    #n90 = all_len[int(ind90[0])]
+        ## index for csumsum >= 0.9*N
+        #csumn90=min(csum[csum >= nx90])
+        #ind90=numpy.where(csum == csumn90)
+        #n90 = all_len[int(ind90[0])]
+
+    except Exception as e:
+        raise Exception(f'Error in calculating stats of {fasta_file}') from e
 
 
 
